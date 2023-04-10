5d56043860970e8c22def7b397c57861c054421f
Rssnobody <silvia.cmilanska@gmail.com>
2023-04-10 22:17:56 +0200

    Initial commit

---
+++ src/main/java/cz/upce/NNPIA_Cmilanska_SEM_BE/NnpiaCmilanskaSemBeApplication.java
@@ -0,0 +1,13 @@
+package cz.upce.NNPIA_Cmilanska_SEM_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnpiaCmilanskaSemBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnpiaCmilanskaSemBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/upce/NNPIA_Cmilanska_SEM_BE/NnpiaCmilanskaSemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.NNPIA_Cmilanska_SEM_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpiaCmilanskaSemBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
